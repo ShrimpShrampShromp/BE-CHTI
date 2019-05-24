e76e545efb99ca0dd76ae1f3b7fa0b4955077053
Louis Chauvet <lchauvet@insat.insa-toulouse.fr>
2019-05-24 17:14:24 +0200

    pour matthieu et clarisse :)

--- ASM/FINAL/Obj/principal.d
+++ ASM/FINAL/Obj/principal.d
@@ -9,3 +9,6 @@
 .\obj\principal.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_compiler.h
 .\obj\principal.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.3.0\CMSIS\Include\cmsis_armcc.h
 .\obj\principal.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h
+.\obj\principal.o: Src\../Affichage_Valise.h
+.\obj\principal.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\time.h
+.\obj\principal.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
--- ASM/FINAL/Src/principal.c
+++ ASM/FINAL/Src/principal.c
@@ -1,5 +1,8 @@
 #include "../etat.h"
 #include "../gassp72.h"
+#include "../Affichage_Valise.h"
+#include <time.h>
+#include <stdlib.h>
 
 unsigned int M2(short *,int);
 volatile unsigned short dma_buf[64];
@@ -8,6 +11,25 @@ int SYSTICK_PER = 5*72000; //5ms en ticks d'horloge à 72MHz
 int indices_k[6]={17,18,19,20,23,24};
 int M2TIR = 2000000; //4194304 = ((124 * (2**15 * 2**5)**2) / 2**32 <- 2**7 en sortie de l'ADC, 2**15 plage de tabcos, 2**5 = nb de points d'aquisition/2, au carré parce que modcarré, divisé par 2**32 quand on prend les bits de poids fort
 int score[6]={0,0,0,0,0,0};
+
+//variables pour le son
+void callback(void );
+extern short Son;
+extern int LongueurSon;
+extern int PeriodeSonMicroSec;
+int resolution;
+int i=0;
+type_etat StructSon;
+
+//variables malette
+
+//CORRESPONDANCE LED/CIBLE :
+// led 2 -> cible 
+// led  -> cible 2
+
+int rand_cible;
+
+
 
 void sys_callback(){
 	GPIO_Set(GPIOB, 1);
@@ -24,6 +46,20 @@ void sys_callback(){
 		}
 		if (compteurs[i] == 3) {
 			score[i]++;
+			StructSon.position=0;
+			//envoi de l'information à la valise
+					
+			Prepare_Afficheur(i, score[i]);
+			Prepare_Clear_LED(i);
+			
+			//nouvelle cible aléatoirement choisie
+			
+			Prepare_Clear_LED(i);
+		  rand_cible = rand() % 4;
+		  Prepare_Set_LED(rand_cible);
+			Choix_Capteur(rand_cible);
+			
+
 		} 
 	}
 	GPIO_Clear(GPIOB, 1);
@@ -33,14 +69,30 @@ void sys_callback(){
 
 int main(void)
 {
+StructSon.position=LongueurSon-1;
+	StructSon.taille=LongueurSon;
+	StructSon.son=&Son;
+	StructSon.periode_ticks=72*PeriodeSonMicroSec;
 // activation de la PLL qui multiplie la fréquence du quartz par 9
 CLOCK_Configure();
 // PA2 (ADC voie 2) = entrée analog
 GPIO_Configure(GPIOA, 2, INPUT, ANALOG);
+// config port PB1 pour être utilisé en sortie (son)
+GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
+StructSon.resolution = PWM_Init_ff( TIM3, 3, StructSon.periode_ticks/3 );
 // PB1 = sortie pour profilage à l'oscillo
 GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
 // PB14 = sortie pour LED
 GPIO_Configure(GPIOB, 14, OUTPUT, OUTPUT_PPULL);
+
+// initialisation du timer 4
+// Periode_en_Tck doit fournir la durée entre interruptions,
+// exprimée en périodes Tck de l'horloge principale du STM32 (72 MHz)
+Timer_1234_Init_ff( TIM4, StructSon.periode_ticks );
+	
+// lancement du timer
+Active_IT_Debordement_Timer( TIM4, 2, callback );
+Run_Timer( TIM4 );
 
 // activation ADC, sampling time 1us
 Init_TimingADC_ActiveADC_ff( ADC1, 72 );
@@ -57,8 +109,20 @@ Systick_Period_ff( SYSTICK_PER );
 Systick_Prio_IT( 3, sys_callback );
 SysTick_On;
 SysTick_Enable_IT;
+
+//paramétrage de la valise
+Init_Affichage();
+
+//on teste d'abord un allumant une seule cible
+
+//rand_cible = rand() % 4;
+Choix_Capteur(2);
+Prepare_Set_LED(2);
+
 	while(1) {
 		int i;
+			Mise_A_Jour_Afficheurs_LED();
+		
 		for(i=0;i<6;i++){
 			if(compteurs[i]>=3){
 				GPIO_Set(GPIOB, 14);
